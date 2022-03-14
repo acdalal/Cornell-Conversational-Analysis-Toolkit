f43cade5fac07ea9d5feeb6bda354c04ba8988a4
roskopfe <roskopfe@carleton.edu>
2022-03-14 13:40:36 -0500

    displacy images to show dependency

--- convokit/testingwork/anonymize.py
+++ convokit/testingwork/anonymize.py
@@ -3,8 +3,12 @@ from convokit.text_processing import textParser, TextProcessor
 # convokit documentation on textparser is useful, tells what tok, rt, etc is
 # the documentation on the phrasing motifs is less so
 import spacy
+from spacy import displacy
+#visualizes spacy parses to see dependency
 import nltk
 import re
+import json
+import pandas as pd
 
 
 def preprocess_text(text):
@@ -17,10 +21,16 @@ def preprocess_text(text):
 
 prep = TextProcessor(proc_fn=preprocess_text, output_field='clean_text')
 nlp = spacy.load('en_core_web_sm')
-parse = textParser.process_text('Hello my name is Emma', mode='parse', sent_tokenizer=None, spacy_nlp = nlp)
+string = 'Hello my name is Emma'
+parse = textParser.process_text(string, mode='parse', sent_tokenizer=None, spacy_nlp = nlp)
 print("Parsed version of: Hello my name is Emma")
 print(censorNouns.censor_nouns(parse))
-parse_ticket = textParser.process_text('This ticket is for creation of Dons OnBase account. There is a separate ticket that Les has for creation of the Network ID account and provisioning of it.  Please follow up with Gayle Bauer with any account information.  Dons Network ID is: dhasseltine,1968', mode='parse', sent_tokenizer=None, spacy_nlp = nlp)
-print("Parsed version of: 'This ticket is for creation of Dons OnBase account. There is a separate ticket that Les has for creation of the Network ID account and provisioning of it.  Please follow up with Gayle Bauer with any account information.  Dons Network ID is: dhasseltine,1968")
-print(censorNouns.censor_nouns(parse_ticket))
+#displacy.serve(nlp(string), style="dep")
+onbase = 'This ticket is for creation of Dons OnBase account. There is a separate ticket that Les has for creation of the Network ID account and provisioning of it.  Please follow up with Gayle Bauer with any account information.  Dons Network ID is: dhasseltine,1968'
+parse_ticket = textParser.process_text(onbase, mode='parse', sent_tokenizer=None, spacy_nlp = nlp)
+displacy.serve(nlp(onbase), style="dep")
+print(onbase)
+parsed_list = censorNouns.censor_nouns(parse_ticket)
+df = pd.DataFrame(parsed_list)
+print(df.head())
 #it's very hard to understand these but there does seem to be 2 NN in my simple statement
